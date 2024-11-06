1916408cb4c639a4eb410ba891c1b1abf3a355c0
Marcin Tatarski <Marcin.Tatarski@joerns.co.uk>
2024-11-06 14:56:20 +0000

    add console app, caclulate given fahrenheit to celsius

---
+++ 1_ConsoleApps/Fahrenheit_to_Celsius/Program.cs
@@ -0,0 +1,6 @@
+﻿/* Calculate Celsius given the current temperature in Fahrenheit */
+
+int fahrenheit = 94;
+decimal convetTempToCelsius = (fahrenheit - 32) * (5m/9);
+
+Console.WriteLine($"The fahrenheit temperature of {fahrenheit} is: {Math.Round(convetTempToCelsius, 1)} celsius");
