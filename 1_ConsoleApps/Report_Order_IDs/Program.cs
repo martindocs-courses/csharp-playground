da7615a0054c9e2c42b2c6ff6db749666ae7c89e
Marcin Tatarski <Marcin.Tatarski@joerns.co.uk>
2024-11-12 11:51:39 +0000

    Add Report Order Ids console app

---
+++ 1_ConsoleApps/Report_Order_IDs/Program.cs
@@ -0,0 +1,14 @@
+﻿
+/*
+    Outputs the Order ID of new orders where the Order ID starts with the letter "B"
+ */
+
+string[] ids = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (var id in ids)
+{
+    if (id.StartsWith('B'))
+    {
+        Console.WriteLine($"Fraud {id} order!");
+    }
+}
