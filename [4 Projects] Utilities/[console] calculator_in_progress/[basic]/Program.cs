6398261e05ba97b8332d411992416cc4525c484d
Marcin Tatarski <Marcin.Tatarski@joerns.co.uk>
2025-01-10 11:35:41 +0000

    Added recursion Restart method fro user who wants to rerun application for next calculation.

--- [4 Projects] Utilities/[console] calculator_in_progress/[basic]/Program.cs
+++ [4 Projects] Utilities/[console] calculator_in_progress/[basic]/Program.cs
@@ -40,135 +40,170 @@
         - for edge case when the user input lower number than zero, example: -18. modify value to absolute value 
         - calculate the values with choosen operator 
        
-    * Return the whole number or decimal value to the user    
+    * Return the whole number or decimal value, depend of the user input        
+    * Ask user if he/she wants to proceede with another calculation:
+        - promt the user to enter 'y' or 'n'
+        - if 'y' use recursive method to repeat calculations
+        - if 'n' or any other character, exit the program
  */
 #endregion
 
-using System.Reflection.Metadata.Ecma335;
 
-double firstValue = UserInput("Enter the first number: ");
-double secondValue = UserInput("Enter the second number: ");
-
-char[] delimiters = { '+', '-', '*', '/' };
-char operatorResult = ' ';
-bool validOperator = false;
-
-// Loop until a valid operator is entered by the user.
-do
+class BasicCalculator
 {
-    Console.Write("Enter the operator ['+', '-', '*', '/']: ");
-    string? inputOperator = Console.ReadLine();
 
-    if (inputOperator != null) {
-        char inputChar = inputOperator.Trim()[0];
-        bool isDelimiter = Array.Exists(delimiters, delimiter => delimiter == inputChar);
-               
-        if (isDelimiter) {
-            operatorResult = inputChar;
-            validOperator = true;
-        }else{
-            Console.WriteLine("\nPlease enter a valid operator.\n");
-        }  
+    static void Main(){
+        Calculator();
     }
-} while (!validOperator);
 
-// Perform the operation based on the selected operator.
-switch (operatorResult){
-    case ('+'):
-        Addition();
-        break;
-    case ('-'):
-        Subtraction();
-        break;
-    case ('*'):
-        Multiplication();
-        break;
-    case ('/'):
-        try{
-            Division();
-        }catch(DivideByZeroException ex){
-            Console.WriteLine($"\n{ex.Message}");
-        }
+    static void Calculator(){
+    
+        double firstValue = UserInput("Enter the first number: ");
+        double secondValue = UserInput("Enter the second number: ");
 
-        break;
-}
+        char[] delimiters = { '+', '-', '*', '/' };
+        char operatorResult = ' ';
+        bool validOperator = false;
 
-// Prompts the user to input a valid number and validates the input.
-double UserInput(string text){
-
-    bool validNumber = false;
-    double result = 0;
-
-    // Invalid input prompts the user to retry.
-    do
-    {
-        Console.Write($"{text}: ");
-        string? number = Console.ReadLine();
-
-        if (number != null)
+        // Loop until a valid operator is entered by the user.
+        do
         {
-            if (double.TryParse(number, out result))
-            {
-                validNumber = true;
+            Console.Write("Enter the operator ['+', '-', '*', '/']: ");
+            string? inputOperator = Console.ReadLine();
+
+            if (inputOperator != null) {
+                char inputChar = inputOperator.Trim()[0];
+                bool isDelimiter = Array.Exists(delimiters, delimiter => delimiter == inputChar);
+               
+                if (isDelimiter) {
+                    operatorResult = inputChar;
+                    validOperator = true;
+                }else{
+                    Console.WriteLine("\nPlease enter a valid operator.\n");
+                }  
             }
-            else
+        } while (!validOperator);
+
+        // Perform the operation based on the selected operator.
+        switch (operatorResult){
+            case ('+'):
+                Addition(firstValue, secondValue);
+                break;
+            case ('-'):
+                Subtraction(firstValue, secondValue);
+                break;
+            case ('*'):
+                Multiplication(firstValue, secondValue);
+                break;
+            case ('/'):
+                try{
+                    Division(firstValue, secondValue);
+                }catch(DivideByZeroException ex){
+                    Console.WriteLine($"\n{ex.Message}");
+                }
+                break;
+            
+        }
+
+        Restart();
+    }
+
+    // Prompts the user to input a valid number and validates the input.
+    static double UserInput(string text){
+
+        bool validNumber = false;
+        double result = 0;
+
+        // Invalid input prompts the user to retry.
+        do
+        {
+            Console.Write($"{text}: ");
+            string? number = Console.ReadLine();
+
+            if (number != null)
             {
-                Console.WriteLine("\nPlease input a valid number.\n");
+                if (double.TryParse(number, out result))
+                {
+                    validNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease input a valid number.\n");
+                }
+            }
+        } while (!validNumber);
+
+        // Return the absolute value of the input to avoid negative values if desired.
+        return AbsoluteValue(result);
+    }
+
+    // Performs addition of the two user inputs and displays the result.
+    static void Addition(double number1, double number2){
+        double result = number1 + number2;
+
+        Console.WriteLine($"The result of {number1} + {number2} = {PrecisionChecks(result)}");
+    }
+
+    // Performs subtraction of the two user inputs and displays the result.
+    static void Subtraction(double number1, double number2)
+    {
+        double result = number1 - number2;
+
+        Console.WriteLine($"The result of {number1} - {number2} = {PrecisionChecks(result)}");
+    }
+
+    // Performs multiplication of the two user inputs and displays the result.
+    static void Multiplication(double number1, double number2)
+    {
+        double result = number1 * number2;
+
+        Console.WriteLine($"The result of {number1} * {number2} = {PrecisionChecks(result)}");
+    }
+
+    // Performs division of the two user inputs and displays the result.
+    static void Division(double number1, double number2)
+    {
+        // Division by zero with double inputs normally results in infinity
+        double result = number1 / number2;
+
+        // Handle divide-by-zero for integer-equivalent second values.
+        // We're explicitly throwing an exception for user feedback if the second number is zero
+        if ((int)number2 == 0){
+            throw new DivideByZeroException("DivideByZeroException: Calculation in 'Division' method encountered an unexpected divide by zero.");
+        }else{
+            Console.WriteLine($"The result of {number1} / {number2} = {PrecisionChecks(result)}");
+        }
+    }
+
+    static double AbsoluteValue(double vale){
+        return Math.Abs(vale);
+    }
+
+    // If calculation of two numbers has any digits after the decimal point, the return number display tree digit after decimal point. Othervise the return value is whole number.  
+    static string PrecisionChecks(double value){
+        if (value % 1 == 0)
+        {
+            return $"{(value):N0}"; ;
+        }
+        else
+        {
+            return $"{(value):N3}";
+        }
+    }
+
+    // Restart the app
+    static void Restart(){
+        Console.WriteLine("\nDo you want to perform another calculation? (yes/no)");
+        string? restart = Console.ReadLine();
+
+        if(restart?.ToLower() != null){
+            if(restart == "y"){
+                Console.Clear();
+                Calculator();
+            }else{
+                Console.WriteLine("Goodbaye!");
+                return;
             }
         }
-    } while (!validNumber);
-
-    // Return the absolute value of the input to avoid negative values if desired.
-    return AbsoluteValue(result);
-}
-
-// Performs addition of the two user inputs and displays the result.
-void Addition(){
-    double result = firstValue + secondValue;
-
-    Console.WriteLine($"The result of {firstValue} + {secondValue} = {PrecisionChecks(result)}");
-}
-
-// Performs subtraction of the two user inputs and displays the result.
-void Subtraction(){
-    double result = firstValue - secondValue;
-
-    Console.WriteLine($"The result of {firstValue} - {secondValue} = {PrecisionChecks(result)}");
-}
-
-// Performs multiplication of the two user inputs and displays the result.
-void Multiplication(){
-    double result = firstValue * secondValue;
-
-    Console.WriteLine($"The result of {firstValue} * {secondValue} = {PrecisionChecks(result)}");
-}
-
-// Performs division of the two user inputs and displays the result.
-void Division(){
-    // Division by zero with double inputs normally results in infinity
-    double result = firstValue / secondValue;
-
-    // Handle divide-by-zero for integer-equivalent second values.
-    // We're explicitly throwing an exception for user feedback if the second number is zero
-    if ((int)secondValue == 0){
-        throw new DivideByZeroException("DivideByZeroException: Calculation in 'Division' method encountered an unexpected divide by zero.");
-    }else{
-        Console.WriteLine($"The result of {firstValue} / {secondValue} = {PrecisionChecks(result)}");
-    }
-}
-
-double AbsoluteValue(double vale){
-    return Math.Abs(vale);
-}
-
-// If calculation of two numbers has any digits after the decimal point, the return number display tree digit after decimal point. Othervise the return value is whole number.  
-string PrecisionChecks(double value){
-    if (value % 1 == 0)
-    {
-        return $"{(value):N0}"; ;
-    }
-    else
-    {
-        return $"{(value):N3}";
     }
 }
