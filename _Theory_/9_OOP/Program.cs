15af9c5eafce54bed4aab31c78748fa5a47b7e11
Marcin Tatarski <Marcin.Tatarski@joerns.co.uk>
2025-01-30 14:43:43 +0000

    Updated theory oop wirh how to create contructor and class.

--- _Theory_/9_OOP/Program.cs
+++ _Theory_/9_OOP/Program.cs
@@ -45,16 +45,28 @@ Console.WriteLine(pizzaDay2025);
 /* CREATE A CLASS */
 Console.WriteLine("\nCreate a class");
 
+var rectangle1 = new Rectangle1();
+Console.WriteLine(rectangle1.Width);
 
-var rectangle = new Rectangle();
+/* CREATE A NEW CONSTRUCTOR */
+Console.WriteLine("\nCreate a constructor");
 
-Console.WriteLine(rectangle.width);
+var rectangle2 = new Rectangle2(10, 10);
+Console.Write(rectangle2.Width + " ");
+Console.Write(rectangle2.Height);
 
+Console.WriteLine();
+
+var rectangle3 = new Rectangle2(30, 30);
+Console.Write(rectangle3.Width + " ");
+Console.Write(rectangle3.Height);
 
 Console.ReadKey();
-class Rectangle{
+
+// declare a class
+class Rectangle1{
     // declare a FIELD/ATTRIBUTE, which is a variable that belongs to an object of a class
-    public int width; // if we dont initialize the field it will be automatically set to the default value for its type, in our case int is '0' (zero)
+    public int Width; // if we dont initialize the field it will be automatically set to the default value for its type, in our case int is '0' (zero). For normal variables that are not belong to class they still be not initialize.
     int height;
 
     // CONSTRUCTOR is a method for instantiate object of the class. It needs have the same name as class and it cannot have return type 
@@ -74,5 +86,51 @@ class Rectangle{
     }
 }
 
-// when defining a class we cant put any code below as it wont work
-//Console.WriteLine("Hello"); // that wont work it needs to before the class definition
+class Rectangle2{
+
+    // Declare fields with as public. it should start with capital letter
+    public int Width;
+    public int Height;
+
+    // privite variable should start with underscore and folow by small letter
+    private int _length;
+
+    /* FIELD INITIALIZATION. We could also declare and initialize values without constractor, but in this case we can only crate new object with already defined (DEFAULT) values;
+        public int Width = 15;
+        public int Height = 15;
+
+      Use when:
+        - The field has a default value that doesn’t change.
+        - You don’t need to perform any validation or complex logic when creating the object.
+        - You want to avoid the need for a constructor when there are no special requirements.
+    */
+
+    /* 
+        Define a constructor:
+        * which need to be named the same as the class
+        * no void or any returning type 
+        * constructor canr only be declared in the class and can't called in the method
+     */
+    public Rectangle2(int width, int height)
+    {
+        // Most of the time that all we do in the constructor is to assign field to parameters.
+        // You can pass values to the constructor, perform validation, or even have different ways of initializing the object based on conditions.
+
+        /*
+            CONSTRUCTOR INITIALIZATION (preferred in most cases). 
+            - You need to pass values to the object upon creation and want to ensure they are valid.
+            - The object’s fields need to be set at the time of creation and don’t have a sensible default value.
+            - You need to perform validation or additional logic before setting the values.
+            - You want to ensure the object is in a valid state when it is created.         
+         */
+        Width = width;
+        Height = height;
+    }
+}
+
+/* 
+    * When defining a class we can't put any executable (top-level statements) code below class declaration, as it won't work:
+        Console.WriteLine("Hello"); // that wont work it needs to before the class definition
+
+    * Top-level statements can only appear before any class or namespace declaration. Once you declare a class (or namespace), the compiler expects that all executable code should be inside methods, constructors, or other members of those classes.
+*/
