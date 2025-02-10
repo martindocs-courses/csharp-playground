db7bb6b07e42942f8ed46720f874766df9136c0e
Marcin Tatarski <Marcin.Tatarski@oxfordhealthcare.co.uk>
2025-02-10 12:54:21 +0000

    Add Single Responsibility Principle version 2 to EducationalDemos.

---
+++ [9 Projects] EducationalDemos/[console] single_responsibility_principle_v2/NamesValidator.cs
@@ -0,0 +1,12 @@
+﻿// Type not placed in any named NAMESPACE belongs to the global namespace
+class NamesValidator
+{
+    public bool IsValid(string name)
+    {
+        return
+            name.Length >= 2 &&
+            name.Length < 25 &&
+            char.IsUpper(name[0]) &&
+            name.All(char.IsLetter);
+    }
+}
