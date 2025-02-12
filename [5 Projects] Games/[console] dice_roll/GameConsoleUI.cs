44a7425582542f8e37e303b62d274794be6d3042
Marcin Tatarski <Marcin.Tatarski@oxfordhealthcare.co.uk>
2025-02-12 12:45:38 +0000

    Add console Roll Dice game using OOPP principles.

---
+++ [5 Projects] Games/[console] dice_roll/GameConsoleUI.cs
@@ -0,0 +1,53 @@
+﻿// GameConsoleUI handles the user interface and interaction with the game
+class GameConsoleUI
+{    
+    private readonly Game _game = new Game(); // Game instance to handle game logic     
+
+    public void GameUI()
+    {
+        // Game loop: keeps asking user if they want to play again after each game
+        while (true)
+        {
+            Console.WriteLine("Dice rolled. Guess what number in 3 tries.");
+            _game.UserInput(); // Initiates the game logic where user guesses the number
+
+            // Check if user wants to exit the game. If yes omit asking user to start again 
+            if (!_game.IsExitGame){
+                Console.WriteLine("Do you want start again? [y/n]");
+                var userInput = Console.ReadLine();
+
+                if(GameRestartConfirmation(userInput)) continue;
+
+                if (ShouldExit(userInput)) break;           
+            }else{
+                break; // end the game 
+            }
+        }
+
+        Console.WriteLine("Goodbye!");
+
+    }
+
+    // Helper method to confirm if the user wants to restart the game
+    private bool GameRestartConfirmation(string userInput)
+    {
+        if (!string.IsNullOrEmpty(userInput) && userInput.Trim().ToLower() == "y")
+        {
+            _game.ResetTries(); // Reset the game tries counter
+            Console.Clear(); // Clear the console for a fresh start
+            return true;
+        }
+
+        return false;
+    }
+
+    // Helper method to check if the user wants to exit the game
+    private bool ShouldExit(string userInput){
+        if (userInput?.Trim().ToLower() != "y"){
+            return true; // User chooses to exit the game
+        }
+
+        return false;
+    }
+
+}
---
+++ [5 Projects] Games/[console] dice_roll/Program.cs
@@ -0,0 +1,22 @@
+﻿
+/* ROLL A DICE */
+#region Pseudocode
+/* 
+    * generate the number from 1 to 6
+    * ask the user to guess the number (user has 3 tries)
+    * if the user input the number
+        * check if the input is a number and if is between 1 to 6
+            * if it's check if is the same as random number
+                * if is right show "You win" and end the game. Optional as user if he/she wants to try again.
+            * if is not, show "Wrong number" and ask user to enter again (check how many tries he/she has left)          
+        * if is not a number 
+            * tell the user that input must be a number and between 1 to 6, and show "Enter a number". Check how many tries user has left
+        * if user didn't guess the number in 3 tries show "You loose" and end the game. Optional as user if he/she wants to try again 
+ */
+#endregion
+
+// Entry point of application
+var user = new GameConsoleUI();
+user.GameUI();
+
+
