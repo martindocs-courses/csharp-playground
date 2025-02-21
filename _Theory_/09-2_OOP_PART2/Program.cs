e28471b19fe5ae3945f1ed09edf7cb75fbd35a84
Marcin Tatarski <Marcin.Tatarski@oxfordhealthcare.co.uk>
2025-02-21 11:47:47 +0000

    Update theory oop part 2 with overriding the base class, inheritance hierarchy.

--- _Theory_/09-2_OOP_PART2/Program.cs
+++ _Theory_/09-2_OOP_PART2/Program.cs
@@ -98,28 +98,37 @@
 
         Note: If you don't want other classes to inherit from a class, use the 'sealed' keyword
  */
-using polyMorphism = PolymorphismTheory;
-using inheritAnce = InheritanceTheory;
+using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Xml.Linq; 
 
+using inherit = Inheritance;
+using inheritMemberProp = InheritanceMembersProps;
+using overridingMembers = OverridingBaseMembers;
+using InheritanceHierarchy;
+
+/* INHERITANCE */
 // ONE OBJECT (interface) to entities of different types 
 Console.WriteLine("Polymorphism: One object (interface) to entities of different types:");
-var pizza = new polyMorphism.Pizza();
-pizza.AddIngredients(new polyMorphism.Cheddar());
-pizza.AddIngredients(new polyMorphism.Mozzarella());
+var pizza = new inherit.Pizza();
+pizza.AddIngredients(new inherit.Cheddar());
+pizza.AddIngredients(new inherit.Mozzarella());
 var showUs = pizza.Describe(); 
 Console.WriteLine(showUs); // Cheddar
 
 // ACCESS TO METHODS - Derived class have access to base class public and protected methods, but not private ones.
 Console.WriteLine("\nAccess to Methods");
-var cheddar = new inheritAnce.Cheddar();
+var cheddar = new inheritMemberProp.Cheddar();
 Console.WriteLine(cheddar.PublicMethod()); // Even if this method is not defined directly in the general class, we can still call it on a cheddar object because it is inherited from the base Ingredient class.
 cheddar.UseMethodFromBaseClass(); // or we could also use it directly within the cheddar class.
+
+// PRIVATE & PROTECTED METHODS - not inherited
 //cheddar.PrivateMethod(); // not accessible
-//cheddar.ProtectedMethod(); // not accessible
+//cheddar.ProtectedMethod(); // not accessible outside only within derived class
 
 // ACCESS to FIELDS - Inherited fields have independent values from base class
 Console.WriteLine("\nAccess to Fields");
-var ingridient = new inheritAnce.Ingredient();
+var ingridient = new inheritMemberProp.Ingredient();
 ingridient.PublicField = 10;
 cheddar.PublicField = 2;
 
@@ -127,17 +136,89 @@ Console.WriteLine("Value of ingredient: " + ingridient.PublicField);
 Console.WriteLine("Value of cheddar: " + cheddar.PublicField);
 
 Console.WriteLine("\nAccess to public fields form derived class");
-var newCheddar1 = new inheritAnce.Cheddar(); // implicit type of Cheddar
-inheritAnce.Cheddar newCheddar2 = new inheritAnce.Cheddar(); // explicit type of Cheddar
+var newCheddar1 = new inheritMemberProp.Cheddar(); // implicit type of Cheddar
+inheritMemberProp.Cheddar newCheddar2 = new inheritMemberProp.Cheddar(); // explicit type of Cheddar
 Console.WriteLine(newCheddar1.Name);
 
-// We can assign an object of type Cheddar to a variable of type Ingredient
-inheritAnce.Ingredient cheddarIngredient = new inheritAnce.Cheddar();
-Console.WriteLine(cheddarIngredient.Name); // if we want to be able to use the Name property on any Ingredient object, we must have this property in the Ingredient class.
+/* OVERRIDING BASE CLASS MEMBERS */
+Console.WriteLine("\nOverriding base class members");
+
+// Since Cheddar is an Ingredient , we should be able to assign an object of type Cheddar to a variable of type Ingredient
+// ingredient is of type of Ingredient, but he can store object of type Cheddar, this works because Cheddar is an ingredient.
+var nonVirtual1 = new overridingMembers.Cheddar();
+Console.WriteLine($"Cheddar type non virtual: {nonVirtual1.Name}"); // => Cheddar cheddar
+
+// if we want to be able to use the Name property on any Ingredient object, we must have this property in the Ingredient class.
+overridingMembers.Ingredient nonVirtual2 = new overridingMembers.Cheddar(); 
+Console.WriteLine($"Ingredient type non virtual: {nonVirtual2.Name}"); // => "Some ingredient"
+
+/*
+    Ingredient ingredient = new Cheddar();
+    Console.WriteLine(ingredient.Name);
+
+    We're getting diffrent result above because when we have a variable of a specific type, and we call some method or property on it, the internal engine of C# does this:
+
+    * Checks if the method/prop is virtual
+    * 
+        ingredient.Name => in our example is not virtual
+
+    * If not, it simply calls the method it finds in the type of the variable in our example is Ingredient type
+    * 
+        ingredient.Name => "Some ingredient"
+
+    * If the method is virtual, it checks the actual type of the object stored in the variable. In this case the type of object stored in the variable is Cheddar type derived from Ingredient type.
+      The C# engine will call SomeMethod/prop defined in this Cheddar type if it overrides the base type method. If not, the implementation from the base class (Ingredient) will be used.
+
+        ingredient.Name => Cheddar cheddar    
+
+    VIRTUAL - means that this method or property may be modified in the inheriting types.
+ */
+Console.WriteLine();
+
+var virtual1 = new overridingMembers.Cheddar();
+Console.WriteLine($"Cheddar type virtual: {virtual1.Name2}"); // => Cheddar cheddar
+
+// if we want to be able to use the Name property on any Ingredient object, we must have this property in the Ingredient class.
+overridingMembers.Ingredient virtual2 = new overridingMembers.Cheddar();
+Console.WriteLine($"Ingredient type virtual: {virtual2.Name2}"); // => Cheddar cheddar
+
+// Base class that store diffrent types, where we want diffrent behavior for each types it stores
+var ingredients = new List<overridingMembers.Ingredient>{ // A List must store objects of uniform type.
+    new overridingMembers.Cheddar(),
+    new overridingMembers.Mozzarella(),
+    new overridingMembers.TomatoSauce(),
+    // Each of them has the Name2 property. The Name2 property is declared as public in the Ingredient class. So every type we have that is an Ingredient also exposes this property as part of its interface.
+};
+
+Console.WriteLine();
+// We want the C# engine to check the actual type of the object assigned to the loop variable in each iteration and call the method from the specific type.
+foreach (overridingMembers.Ingredient ingredient in ingredients) // loop of Ingredient type
+{
+    Console.WriteLine(ingredient.Name2);
+}
+
+/* INHERITANCE HIERARCHY */
+Console.WriteLine("\nInheritance hierarchy");
+var inheritHierarchy = new Pizza();
+
+/*
+We can still pass Mozzarella and Cheddar to the AddIngredient method, taking an Ingredient as a parameter. This is because both Mozzarella and Cheddar still inherit from the Ingredient class. So they are ingredients.
+
+The only difference is that the inheritance is no longer direct. 
+    Now goes through:   Cheddar => Cheese => Ingredient
+    Before:             Cheddar => Ingredient
+
+*/
+inheritHierarchy.AddIngredients(new Cheddar()); 
+inheritHierarchy.AddIngredients(new Mozzarella()); 
+
+//inheritHierarchy.AddIngredients(new TomatoSauce());
+
+Console.WriteLine(inheritHierarchy.Describe());
 
 Console.ReadKey();
 
-namespace PolymorphismTheory
+namespace Inheritance
 {
     public class Pizza
     {
@@ -145,8 +226,9 @@ namespace PolymorphismTheory
         private List<Ingredient> _ingridients = new List<Ingredient>();
 
         // A method that adds a new ingredient to a pizza.
-        public void AddIngredients(Ingredient ingredient) => _ingridients.Add(ingredient);
-
+        public void AddIngredients(Ingredient ingredient) =>
+            _ingridients.Add(ingredient);
+        
         // Describe method listing all the ingredients.
         public string Describe() => $"This pizza is with a {string.Join(',', _ingridients)}";
     }
@@ -154,13 +236,13 @@ namespace PolymorphismTheory
     // base class
     public class Ingredient
     {
-                
+        
     }
 
     // derived class
     public class Cheddar : Ingredient
-    {
-
+    {        
+        // read only (getter) prop Name using Expression-Bodied Property
         public string Name => "Cheddar cheese";
         public int AgedForMonths { get; }                
     }
@@ -180,7 +262,7 @@ namespace PolymorphismTheory
     }
 }
 
-namespace InheritanceTheory
+namespace InheritanceMembersProps
 {
     public class Pizza
     {
@@ -196,12 +278,13 @@ namespace InheritanceTheory
 
     // base class
     public class Ingredient
-    {
-
-        public string Name { get; } = "Some ingredient";
-
+    {     
         public int PublicField; // derived class have access to base public fields
 
+        // Sould include data and methods common for any ingredient 
+        // Usually contains basic implementation
+
+        // accessible for any derived class
         public string PublicMethod() => "This method is PUBLIC in the Ingredient class.";
 
         // Unlike public methods, private ones are not inherited.
@@ -214,6 +297,9 @@ namespace InheritanceTheory
     // derived class
     public class Cheddar : Ingredient
     {
+        // The base type contains the data and methods common to the related types.         Still, each derived type can have its own members, which the base type and other derived types do not contain.
+
+        // Then Derived class may contain more specific implementation instead of using the one from the base class.
 
         // Fields that are public or protected also get inherited by derived classes.
         /*
@@ -222,12 +308,12 @@ namespace InheritanceTheory
         public string Name => "Cheddar cheese";
         public int AgedForMonths { get; }
 
-        // use Ingredient method directly within the cheddar class.
+        // We can also use Ingredient method directly within the cheddar class.
         public void UseMethodFromBaseClass()
         {
             Console.WriteLine(PublicMethod());
-            //Console.WriteLine(PrivateMethod()); // wont work
-            Console.WriteLine(ProtectedMethod());
+            //Console.WriteLine(PrivateMethod()); // wont work, the merhods are not inherited
+            Console.WriteLine(ProtectedMethod()); // will work, but only withwin Derived class, not outside
 
         }
     }
@@ -247,6 +333,117 @@ namespace InheritanceTheory
     }
 }
 
+namespace OverridingBaseMembers{
+    public class Pizza
+    {        
+        private List<Ingredient> _ingredients = new List<Ingredient>();
+        
+        public void AddIngredients(Ingredient ingredient) => _ingredients.Add(ingredient);
+               
+        public string Describe() => $"This pizza is with a {string.Join(',', _ingredients)}";
+    }
+
+    // base class
+    public class Ingredient
+    {
+
+        public string Name { get; } = "Some ingredient";
+        public virtual string Name2 { get; } = "Some ingredient";
+        public string PublicMethod() => "This method is PUBLIC in the Ingredient class.";
+    }
+
+    // derived class
+    public class Cheddar : Ingredient
+    {
+        
+        public string Name => "Cheddar cheese";
+
+        // his prop hides the base type property when not override word is used
+        public override string Name2 => "Cheddar cheese"; 
+        public int AgedForMonths { get; }
+
+       
+        public void UseMethodFromBaseClass()
+        {
+            Console.WriteLine(PublicMethod());         
+          
+        }
+    }
+
+    // derived class
+    public class TomatoSauce : Ingredient
+    {
+        public string Name => "Tomato sauce";
+        public string Name2 => "Tomato sauce";
+        public int TomatoIn100Grams { get; }
+    }
+
+    // derived class
+    public class Mozzarella : Ingredient
+    {
+        public string Name => "Mozzarella";
+        public override string Name2 => "Mozzarella";
+        public bool IsLight { get; }
+    }
+}
+
+namespace InheritanceHierarchy{
+    public class Pizza
+    {
+        private List<Ingredient> _ingredients = new List<Ingredient>();
+
+        public void AddIngredients(Cheese ingredient) => _ingredients.Add(ingredient);
+
+        public string Describe() => $"This pizza is with a {string.Join(',', _ingredients)}";
+    }
+
+    // base class
+    public class Ingredient
+    {
+                
+        public virtual string Name { get; } = "Some ingredient";
+        public string PublicMethod() => "This method is PUBLIC in the Ingredient class.";
+    }
+
+    // Sometimes we want the inheritance hierarchy to contain more than two types.
+    // Both cheddar and mozzarella are types of cheese. Instead of making them inherit from the Ingredient class, we can derive them from the Cheese class.
+    public class Cheese: Ingredient
+    {
+
+    }
+
+    // derived class
+    public class Cheddar : Cheese
+    {
+                     
+        public override string Name => "Cheddar cheese";
+        public int AgedForMonths { get; }
+
+
+        public void UseMethodFromBaseClass()
+        {
+            Console.WriteLine(PublicMethod());
+
+        }
+    }
+      
+
+    // derived class
+    public class Mozzarella : Cheese
+    {
+        public override string Name => "Mozzarella";
+        public bool IsLight { get; }
+    }
+
+    // derived class
+    public class TomatoSauce : Ingredient
+    {
+        public override string Name => "Tomato sauce";
+        public int TomatoIn100Grams { get; }
+    }
+
+   
+}
 /* 
     - ABSTRACTION - classes only exposes essential data and methods and hide the underlying details,
 
@@ -265,7 +462,7 @@ namespace InheritanceTheory
 
         From the example above, it is not possible to create an object of the Animal class. To access the abstract class, it must be inherited from another class.
 
-    - INTERFACE - is a completely "abstract class", which can only contain abstract methods and properties (with empty bodies).
+    - INTERFACE - is a completely "abstract class", which can only contain abstract methods and properties (with empty bodies). 
 
         It is considered good practice to start with the letter "I" at the beginning of an interface, as it makes it easier for yourself and others to remember that it is an interface and not a class.
 
