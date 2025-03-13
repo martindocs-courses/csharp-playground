82f881280ba080459230826fbb91377eff3964fc
Marcin Tatarski <martin.tatarski@gmail.com>
2025-03-13 14:40:26 +0000

    Update miscellaneous with extend methods.

---
+++ _Theory_/10_Miscellaneous/Extensions/SeasonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ExtensionMethods;
+namespace _10_Miscellaneous.Extensions
+{
+    public static class SeasonExtensions
+    {
+        public static Season NextSeason(this Season season){
+            int seasonAsInt = (int)season;
+            int nextSeason = (seasonAsInt + 1) % 4; // add +1 to numeric representation of enum value
+              /*
+                    Spring = 0
+                    Summer = 1
+                    Autumn = 2
+                    Winter = 3
+               */
+            return (Season)nextSeason;
+        } 
+
+    }
+}
---
+++ _Theory_/10_Miscellaneous/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace _10_Miscellaneous.Extensions
+{
+    // Extension methods can only be defined in static classes 
+    public static class StringExtensions
+    {
+        // extension method must be always static
+        public static int CountLines(this string input) =>
+            /*
+                * parameter 'string input' is of the type we want extend. 
+                * 'this' modifier must be added when creating extension.
+                * the extended parameter must be first in the list of parameters ... CountLines(this string input, int a) ...
+                * it's good practice to have single extension for each type
+             
+             */
+            input.Split(Environment.NewLine).Length;
+    }
+}
