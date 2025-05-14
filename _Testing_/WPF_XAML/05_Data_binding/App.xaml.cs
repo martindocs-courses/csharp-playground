21d507dc485410e04b97d9ada53c43dfd62c8c76
Marcin Tatarski <martin.tatarski@gmail.com>
2025-05-14 11:03:09 +0100

    Learn how to implement some controls in WPF and how to use XAML

--- _Testing_/WPF_XAML/05_Data_binding/App.xaml.cs
+++ _Testing_/WPF_XAML/05_Data_binding/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace TestApp
+namespace _05_Data_binding
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ _Testing_/WPF_XAML/05_Data_binding/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace DataBinding
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return _boundText; }
+            set { 
+                _boundText = value;
+                // when using OnPropChange() we do not have to relay on GUI to finish logic and vice versa
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+                
+                // We could also create method an use like this
+                //OnPropertyChanged("BoundText");
+                OnPropertyChanged(); // because of using [CallerMemberName] we do not have to pass args as will automatically populate with the caller name for us based on the caller, so if BasedText is the one calling the OnPropertyChanged(); method
+            }
+        }
+
+        public MainWindow()
+        {
+            DataContext = this; // setting the data context prop to 'this' code behind file
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code.";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null){
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
