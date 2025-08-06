e7238bf55a648f83c40fa91eb6634914847da915
Marcin Tatarski <martin.tatarski@gmail.com>
2025-08-06 23:17:03 +0100

    Add Composition practice.

---
+++ _Practice_/09_OOP/6-Composition/Stopwatch_TimeTracker/project1.cs
@@ -0,0 +1,19 @@
+﻿
+namespace OOP_Composition.Stopwatch_TimeTracker
+{
+   
+    public class TimeTracker{
+        public DateTime CurrentTime() => DateTime.Now;
+    }
+
+    public class StopWatch{
+        private readonly TimeTracker _tracker;
+
+        public StopWatch(TimeTracker tracker)
+        {
+            _tracker = tracker;
+        }
+
+        public void TimingInfo() => Console.WriteLine(_tracker.CurrentTime());
+    }
+}
