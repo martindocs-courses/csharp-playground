65dfa634d7a9322d85020922224ee5fad9a38976
Martin Tatarski <martin.tatarski@gmail.com>
2026-02-16 22:37:23 +0000

    Update follow along course.

---
+++ _Practice_/ASP.NET_core/ManagmentSystem/ManagmentSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManagmentSystem.Web.Models;
+
+namespace ManagmentSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult About()
+        {
+            var model = new TestViewModel { Name = "Martin", DateOfBirth = new DateTime(2010, 8, 12) };
+            return View(model);
+        }
+    }
+}
--- _Practice_/ASP.NET_core/ManagmentSystem/ManagmentSystem.Web/Data/ApplicationDbContext.cs
+++ _Practice_/ASP.NET_core/ManagmentSystem/ManagmentSystem.Web/Data/ApplicationDbContext.cs
@@ -9,5 +9,15 @@ namespace ManagmentSystem.Web.Data
             : base(options)
         {
         }
+        // To make aware that EF Core should know about below data madel which os representation of table that we wanted in the database
+        //           data types      name of the table in db
+        //                |             |
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        // After creating above we do next migration: add-migration AddLeaveTypesTable
+
+        // If we do any changes on Model we need to remove migration and again update the database: Remove-Migration
+
+        // and then add the LeaveTypes table to database: update-database
     }
 }
